8727addc80eb9c81a1c6591cbf7fa9a7870144e1
samy senani <aceportgasd235@gmail.com>
2023-02-23 19:12:56 +0100

    edit: dashboard widgets cpt

--- src/components/Dashboard/DashboardWidgets.js
+++ src/components/Dashboard/DashboardWidgets.js
@@ -1,8 +1,8 @@
 import React from "react";
 import LottieMoney from "../LotiIcon/LottieMoney";
-import LottieProduct from "../LotiIcon/LottieProduct";
-import LottieInvoice from "../LotiIcon/LottieInvoice";
-import LottiePersons from "../LotiIcon/LottiePersons";
+// import LottieProduct from "../LotiIcon/LottieProduct";
+// import LottieInvoice from "../LotiIcon/LottieInvoice";
+// import LottiePersons from "../LotiIcon/LottiePersons";
 import { useSelector } from "react-redux";
 import { getAllClientsSelector } from "../../store/clientSlice";
 import { getAllProductSelector } from "../../store/productSlice";
@@ -18,93 +18,66 @@ function DashboardWidgets() {
   const totalBalance = useSelector(getTotalBalance);
   const allInvoices = useSelector(getAllInvoiceSelector);
 
+  const expenses = 0;
+  const incomes = 0;
+
+  const OPTIONS = [
+    {
+      label: "Incomes",
+      icon: <LottieMoney loop className="h-20" />,
+      data: incomes,
+    },
+    {
+      label: "Expenses",
+      icon: <LottieMoney loop className="h-20" />,
+      data: expenses,
+    },
+    {
+      label: "Invoices",
+      icon: <LottieMoney loop className="h-20" />,
+      data: allInvoices?.length,
+    },
+    {
+      label: "Products",
+      icon: <LottieMoney loop className="h-20" />,
+      data: products?.length,
+    },
+    {
+      label: "Clients",
+      icon: <LottieMoney loop className="h-20" />,
+      data: clients?.length,
+    },
+    {
+      label: "Total Balance",
+      icon: <LottieMoney loop className="h-20" />,
+      data: totalBalance,
+    },
+  ];
+
   return (
     <>
       <div className="flex flex-wrap">
-        <div className="w-full mb-3 md:w-1/2">
-          <div className="p-4 bg-white rounded-xl md:mr-4 hover:shadow-sm">
-            <div className="font-title">Total Balance</div>
-            <div className="flex justify-between items-center">
-              {/* Icon */}
-              <div className="h-30">
-                <LottieMoney loop className="h-20" />
-              </div>
-              {/* Icon Finished */}
-              <div className="text-2xl mr-2">
-                <NumberFormat
-                  value={totalBalance}
-                  className=""
-                  displayType={"text"}
-                  thousandSeparator={true}
-                  renderText={(value, props) => <span {...props}>{value}</span>}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-full mb-3 md:w-1/2">
-          <div className="p-4 bg-white rounded-xl hover:shadow-sm">
-            <div className="font-title">Total Products</div>
-            <div className="flex justify-between items-center">
-              {/* Icon */}
-              <div className="h-30">
-                <LottieProduct loop className="h-20" />
-              </div>
-              {/* Icon Finished */}
-              <div className="text-2xl mr-2">
-                <NumberFormat
-                  value={products?.length}
-                  className=""
-                  displayType={"text"}
-                  thousandSeparator={true}
-                  renderText={(value, props) => <span {...props}>{value}</span>}
-                />
+        {OPTIONS.map((item, idx) => (
+          <div key={idx} className="w-full mb-3 md:w-1/2">
+            <div className="p-4 bg-white rounded-xl md:mr-4 hover:shadow-sm">
+              <div className="font-title"> {item?.label} </div>
+              <div className="flex justify-between items-center">
+                <div className="h-30">{item?.icon}</div>
+                <div className="text-2xl mr-2">
+                  <NumberFormat
+                    value={item?.data}
+                    className=""
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    renderText={(value, props) => (
+                      <span {...props}>{value}</span>
+                    )}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="w-full mb-3 md:w-1/2">
-          <div className="p-4 bg-white rounded-xl md:mr-4 hover:shadow-sm">
-            <div className="font-title">Total Invoices</div>
-            <div className="flex justify-between items-center">
-              {/* Icon */}
-              <div className="h-30">
-                <LottieInvoice loop className="h-20" />
-              </div>
-              {/* Icon Finished */}
-              <div className="text-2xl mr-2">
-                <NumberFormat
-                  value={allInvoices?.length}
-                  className=""
-                  displayType={"text"}
-                  thousandSeparator={true}
-                  renderText={(value, props) => <span {...props}>{value}</span>}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-full mb-3 md:w-1/2">
-          <div className="p-4 bg-white rounded-xl hover:shadow-sm">
-            <div className="font-title">Total Clients</div>
-            <div className="flex justify-between items-center">
-              {/* Icon */}
-              <div className="h-30">
-                <LottiePersons loop className="h-20" />
-              </div>
-              {/* Icon Finished */}
-              <div className="text-2xl mr-2">
-                <NumberFormat
-                  value={clients?.length}
-                  className=""
-                  displayType={"text"}
-                  thousandSeparator={true}
-                  renderText={(value, props) => <span {...props}>{value}</span>}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
