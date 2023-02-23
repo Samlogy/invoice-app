48009551bfccab9ce17b0451669c25465b06de5d
samy senani <aceportgasd235@gmail.com>
2023-02-23 19:49:14 +0100

    add: template page --> product / client refactoring: products / clients pages delete: products / clients pages

---
+++ src/pages/products/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TemplateProductClient from "../../components/TemplateProductClient";
+
+export default function ProductListScreen() {
+  return (
+    <TemplateProductClient title="products" />
+  );
+}
