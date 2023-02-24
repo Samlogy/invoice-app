25cf4e717138cfb49f63e0e4aeeee7d47e09d2f7
samy senani <aceportgasd235@gmail.com>
2023-02-24 08:50:54 +0100

    add: income list page

---
+++ src/pages/incomes/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import IncomeExpenseTemplate from '../../components/IncomeExpenseTemplate'
+
+export default  function IncomesList() {
+  return (
+    <IncomeExpenseTemplate title="incomes" />
+  )
+}
