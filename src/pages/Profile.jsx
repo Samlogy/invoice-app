e85c98af14af7eebf68fd34ec7b0d45062870591
samy senani <aceportgasd235@gmail.com>
2023-02-25 10:41:43 +0100

    add: profile page * form (add / edit) profile data * preview profile data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 import Button from "../components/Button/Button";
 import ImageUpload from "../components/Common/ImageUpload";
-import SectionTitle from "../components/Common/SectionTitle";
+import PageTitle from "../components/Common/PageTitle";
 import { getCompanyData, updateCompanyData } from "../store/companySlice";
 import { useAppContext } from "../context/AppContext";
 import {
@@ -23,8 +23,9 @@ const emptyForm = {
   companyEmail: "",
   companyMobile: "",
   billingAddress: "",
+  website: "",
   createdAt: new Date().toDateString(),
-  editedAt: ""
+  editedAt: "",
 };
 
 export default function Profile({ isShowDetail = false, alreadySet = false }) {
@@ -105,104 +106,163 @@ export default function Profile({ isShowDetail = false, alreadySet = false }) {
   }, [companyForm]);
 
   return (
-    <div className="bg-white rounded-xl p-4 mt-4">
-      <SectionTitle> Profile </SectionTitle>
-      <div className="flex mt-2">
-        {isInitLoading ? (
-          <Skeleton className="skeleton-input-radius skeleton-image border-dashed border-2" />
-        ) : (
-          <ImageUpload
-            onChangeImage={onChangeImage}
-            keyName="QuickEditImageUpload"
-            className={imageUploadClasses}
-            url={companyForm.image}
-          />
-        )}
-
-        <div className="flex-1 pl-3">
-          {isInitLoading ? (
-            <Skeleton className={defaultSkeletonLargeStyle} />
-          ) : (
-            <input
-              value={companyForm.companyName}
-              placeholder="Company Name"
-              className={
-                !validForm.companyName && isTouched
-                  ? defaultInputLargeInvalidStyle
-                  : defaultInputLargeStyle
-              }
-              onChange={(e) => handlerCompanyValue(e, "companyName")}
-              disabled={isInitLoading}
-            />
-          )}
+    <div className="flex flex-col-reverse lg:flex-row justify-around w-full">
+      <div className="bg-white rounded-xl p-4 m-4 w-full lg:w-1/3">
+        <div className="p-4">
+          <PageTitle title={"Preview"} />
+          <div className="flex flex-col mt-4">
+            <div className="flex w-full justify-between">
+              <div className={"mb-2"}>
+                {companyForm?.image ? (
+                  <img
+                    className={"object-cover h-14 w-14 rounded-lg"}
+                    src={companyForm?.image}
+                    alt="upload_image"
+                  />
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                    />
+                  </svg>
+                )}
+              </div>
+              <span className="font-medium text-sm"> {!companyForm?.image ? 'Your Company Image' : ""} </span>
+            </div>
+            <div className="flex w-full justify-between">
+              <span className="font-bold mb-2"> Name: </span>
+              <span className="font-medium text-sm"> {companyForm?.companyName || 'Your Company Name'} </span>
+            </div>
+            <div className="flex w-full justify-between">
+              <span className="font-bold mb-2"> Email: </span>
+              <span className="font-medium text-sm"> {companyForm?.companyEmail || 'Your Company Email'} </span>
+            </div>
+            <div className="flex w-full justify-between">
+              <span className="font-bold mb-2"> Mobile: </span>
+              <span className="font-medium text-sm"> {companyForm?.companyMobile || 'Your Company Mobile'} </span>
+            </div>
+            <div className="flex w-full justify-between">
+              <span className="font-bold mb-2"> Address: </span>
+              <span className="font-medium text-sm"> {companyForm?.billingAddress || 'Your Company Address'} </span>
+            </div>
+            <div className="flex w-full justify-between">
+              <span className="font-bold mb-2"> Website: </span>
+              <span className="font-medium text-sm"> {companyForm?.website || 'Your Company Website'} </span>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="flex mt-2">
-        <div className="flex-1">
+      <div className="bg-white rounded-xl p-4 m-4 w-full lg:w-1/2">
+        <div className="p-4">
+          <PageTitle title={"Profile"} />
+        </div>
+        <div className="flex mt-2">
           {isInitLoading ? (
-            <Skeleton className={defaultSkeletonNormalStyle} />
+            <Skeleton className="skeleton-input-radius skeleton-image border-dashed border-2" />
           ) : (
-            <input
-              value={companyForm.billingAddress}
-              placeholder="Company Address"
-              className={
-                !validForm.billingAddress && isTouched
-                  ? defaultInputInvalidStyle
-                  : defaultInputStyle
-              }
-              onChange={(e) => handlerCompanyValue(e, "billingAddress")}
-              disabled={isInitLoading}
+            <ImageUpload
+              onChangeImage={onChangeImage}
+              keyName="QuickEditImageUpload"
+              className={imageUploadClasses}
+              url={companyForm.image}
             />
           )}
-        </div>
-      </div>
 
-      <>
+          <div className="flex-1 pl-3">
+            {isInitLoading ? (
+              <Skeleton className={defaultSkeletonLargeStyle} />
+            ) : (
+              <input
+                value={companyForm.companyName}
+                placeholder="Company Name"
+                className={
+                  !validForm.companyName && isTouched
+                    ? defaultInputLargeInvalidStyle
+                    : defaultInputLargeStyle
+                }
+                onChange={(e) => handlerCompanyValue(e, "companyName")}
+                disabled={isInitLoading}
+              />
+            )}
+          </div>
+        </div>
+
         <div className="flex mt-2">
           <div className="flex-1">
             {isInitLoading ? (
               <Skeleton className={defaultSkeletonNormalStyle} />
             ) : (
               <input
-                value={companyForm.companyEmail}
-                placeholder="Company Email"
+                value={companyForm.billingAddress}
+                placeholder="Company Address"
                 className={
-                  !validForm.companyEmail && isTouched
+                  !validForm.billingAddress && isTouched
                     ? defaultInputInvalidStyle
                     : defaultInputStyle
                 }
-                onChange={(e) => handlerCompanyValue(e, "companyEmail")}
+                onChange={(e) => handlerCompanyValue(e, "billingAddress")}
                 disabled={isInitLoading}
               />
             )}
           </div>
         </div>
-        <div className="flex mt-2">
-          <div className="flex-1">
-            {isInitLoading ? (
-              <Skeleton className={defaultSkeletonNormalStyle} />
-            ) : (
-              <input
-                value={companyForm.companyMobile}
-                placeholder="Company Phone"
-                className={
-                  !validForm.companyMobile && isTouched
-                    ? defaultInputInvalidStyle
-                    : defaultInputStyle
-                }
-                onChange={(e) => handlerCompanyValue(e, "companyMobile")}
-                disabled={isInitLoading}
-              />
-            )}
-          </div>
-        </div>
-      </>
 
-      <div className="mt-3">
-        <Button onClick={submitHandler} block={1}>
-          <span className="inline-block ml-2"> Submit </span>
-        </Button>
+        <>
+          <div className="flex mt-2">
+            <div className="flex-1">
+              {isInitLoading ? (
+                <Skeleton className={defaultSkeletonNormalStyle} />
+              ) : (
+                <input
+                  value={companyForm.companyEmail}
+                  placeholder="Company Email"
+                  className={
+                    !validForm.companyEmail && isTouched
+                      ? defaultInputInvalidStyle
+                      : defaultInputStyle
+                  }
+                  onChange={(e) => handlerCompanyValue(e, "companyEmail")}
+                  disabled={isInitLoading}
+                />
+              )}
+            </div>
+          </div>
+          <div className="flex mt-2">
+            <div className="flex-1">
+              {isInitLoading ? (
+                <Skeleton className={defaultSkeletonNormalStyle} />
+              ) : (
+                <input
+                  value={companyForm.companyMobile}
+                  placeholder="Company Phone"
+                  className={
+                    !validForm.companyMobile && isTouched
+                      ? defaultInputInvalidStyle
+                      : defaultInputStyle
+                  }
+                  onChange={(e) => handlerCompanyValue(e, "companyMobile")}
+                  disabled={isInitLoading}
+                />
+              )}
+            </div>
+          </div>
+        </>
+
+        <div className="mt-3">
+          <Button onClick={submitHandler} block={1}>
+            <span className="inline-block ml-2"> Submit </span>
+          </Button>
+        </div>
       </div>
     </div>
   );
